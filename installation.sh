7512f53df99bec6186039640ea20bf0960749b65
Manish Sharma <62472469+7uc1f3r@users.noreply.github.com>
2020-05-16 20:05:00 +0530

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -42,10 +42,10 @@ function atom_install(){
 
 function sublime_install(){
 	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-	sudo apt-get install apt-transport-https
+	apt install apt-transport-https
 	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-	sudo apt update
-	sudo apt install sublime-text
+	apt update
+	apt install sublime-text
 }
 
 function spotify_install(){
